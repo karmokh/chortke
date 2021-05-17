f68ec52afe39d1091d119788dba43f64e142149a
amirebneali7 <13741384>
2021-05-17 12:02:08 +0430

    edit model name and routes and messages

--- messages/messages.js
+++ messages/messages.js
@@ -1,27 +1,25 @@
 module.exports = (action, attribute, option) => {
-  option = option || null;  
-  switch (action) {
-    case "create":
-      return `${attribute} با موفقیت ایجاد گردید`;
-    case "update":
-      return `${attribute} با موفقیت ویرایش گردید`;
-    case "delete":
-      return `${attribute} با موفقیت حذف گردید`;
-    case "required":
-      return `${attribute} را وارد نمایید`;
-    case "max":
-      return `${attribute} با موفقیت حذف گردید`;
-    case "pattern":
-      return `${attribute} با موفقیت حذف گردید`;
-    case "length":
-      return `${attribute} با موفقیت حذف گردید`;
-    case "unique":
-      return `${attribute} با موفقیت حذف گردید`;
-    case "email":
-      return `${attribute} با موفقیت حذف گردید`;
-    case "equal":
-      return `${attribute} با موفقیت حذف گردید`;
-    case "enum":
-      return `${attribute} با موفقیت حذف گردید`;
-  }
+    option = option || null;
+    switch (action) {
+        case "create":
+            return `${attribute} با موفقیت ایجاد گردید`;
+        case "update":
+            return `${attribute} با موفقیت ویرایش گردید`;
+        case "delete":
+            return `${attribute} با موفقیت حذف گردید`;
+        case "required":
+            return `فیلد ${attribute} اجباری میباشد`;
+        case "max":
+            return `${attribute} نمی تواند بیش از ${option} کاراکتر باشد`;
+        case "pattern":
+            return `${attribute} وارد شده صحیح نمی باشد`;
+        case "unique":
+            return `${attribute} وارد شده تکراری می باشد`;
+        case "email":
+            return `${attribute} وارد شده معتبر نمی باشد`;
+        case "equal":
+            return `${attribute} و ${option} یکسان نمی باشند`;
+        case "enum":
+            return `${attribute} معتبر نمی باشد`;
+    }
 };
